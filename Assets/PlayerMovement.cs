099935e6942de9623a3c1942be8529666e4139b5
Vishal Venkatraman <ihandsomeguy97@gmail.com>
2020-04-23 22:50:37 +0800

    First Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    float horMove = 0f;
+
+    public float runSpeed = 100f;
+
+    bool jump = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        controller.Move(horMove * Time.fixedDeltaTime, false, jump);
+
+        jump = false;
+    }
+}
